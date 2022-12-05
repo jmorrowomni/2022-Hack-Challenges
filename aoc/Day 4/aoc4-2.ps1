388dcd105cc2ee431357141ca92fc150a7efa6d9
Jeremy Morrow <shaddowez@gmail.com>
2022-12-05 12:48:09 -0500

    Completed Day 4

--- aoc/Day 4/aoc4-2.ps1
+++ aoc/Day 4/aoc4-2.ps1
@@ -1,6 +1,5 @@
 $stream_reader = New-Object System.IO.StreamReader{aoc\Day 4\day4input.txt}
 $total = 0
-$i = 1
 
 while (($current_line =$stream_reader.ReadLine()) -ne $null)
 
@@ -11,14 +10,12 @@ while (($current_line =$stream_reader.ReadLine()) -ne $null)
 
     #if (([int]$cleaned1[0] -le [int]$cleaned2[0] -and [int]$cleaned1[1] -ge [int]$cleaned2[1]) -or ([int]$cleaned2[0] -le [int]$cleaned1[0] -and [int]$cleaned2[1] -ge [int]$cleaned1[1])) 
     if (([int]$cleaned1[0] -gt [int]$cleaned2[1]) -or ([int]$cleaned1[1] -lt [int]$cleaned2[0])) {
-        break
     }
     else {
         Write-Host "$cleaned1,$cleaned2"
         $total++
     }
-
-    $i++
+  
 }
 
 Write-Output "$total pairs overlap"
